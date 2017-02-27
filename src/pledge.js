711ce4e65089e19dfaadd9d69e18b57a41df2983
wfgmiami <wfgmiami@yahoo.com>
2017-02-27 08:44:55 -0500

    first

---
+++ src/pledge.js
@@ -0,0 +1,23 @@
+'use strict';
+/*----------------------------------------------------------------
+Promises Workshop: build the pledge.js ES6-style promise library
+----------------------------------------------------------------*/
+// YOUR CODE HERE:
+
+
+
+
+
+
+/*-------------------------------------------------------
+The spec was designed to work with Test'Em, so we don't
+actually use module.exports. But here it is for reference:
+
+module.exports = $Promise;
+
+So in a Node-based project we could write things like this:
+
+var Promise = require('pledge');
+…
+var promise = new Promise(function (resolve, reject) { … });
+--------------------------------------------------------*/
